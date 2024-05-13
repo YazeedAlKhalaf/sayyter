1989ce4fee059784f9b833eaec3bc8b64d71a877
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-05-13 17:46:38 +0300

    big bang

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sayyter - سيطر
+
+A fintech app concept, or maybe one day it becomes a real thing :D
---
+++ sayyter/sayyterApp.swift
@@ -0,0 +1,17 @@
+//
+//  sayyterApp.swift
+//  sayyter
+//
+//  Created by Yazeed AlKhalaf on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct sayyterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
